5c5e02c292b8723629798566e170b720bb9b56cf
Davi Resende <165061039+daviresende2@users.noreply.github.com>
2024-04-02 16:07:51 -0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -2,15 +2,15 @@
         let taskText = taskInput.value.trim();
         if (taskText === '') return;
 
-        // Cria um novo item de lista (li) com input de texto e botões
+        // Cria novo item de lista com input de texto e os botões
         let li = document.createElement('li');
         li.innerHTML = `
             <input type="text" value="${taskText}" readonly>
             <button class="edit" onclick="editTask(this)">Editar</button>
             <button class="delete" onclick="deleteTask(this)">Excluir</button>
         `;
-        taskList.appendChild(li); // Adiciona o novo item à lista
-        taskInput.value = ''; // Limpa o input de texto após adicionar
+        taskList.appendChild(li); // Adiciona o novo item a lista
+        taskInput.value = ''; // Limpa o input de texto ao adicionar
     }
 
     // Função para editar uma tarefa
